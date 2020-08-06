110583dfb23bb1b5fb5f6d40915f5c94421f695a
Nicholas Ryder <nryder7@gmail.com>
2020-08-05 22:51:26 -0400

    countdownTimer

--- Homework3/style.js
+++ Homework3/style.js
@@ -1,13 +1,30 @@
 var startButton = document.querySelector("#start");
-var displayQuestionForm = document.querySelector("#QuestionForm");
-
+var displayQuestionForm = document.querySelector("#questionForm");
+var timer = document.querySelector("#timer");
+var secondsLeft = 60;
 
 
 startButton.addEventListener("click", begin)
  function begin(){
-     if(event.target.matches("button")){
-        startButton.setAttribute("style", "display:none");
-        displayQuestionForm.setAttribute("style", "display:block");
-    
+   if(event.target.matches("button")){
+     startButton.setAttribute("style", "display:none");
+     displayQuestionForm.setAttribute("style", "display:block");
+     timer.textContent = "You have " + secondsLeft + " seconds remaining";
+     console.log(timer);
+
+      function setTime() {
+         var timerInterval = setInterval(function() {
+             secondsLeft--;
+             timer.textContent = "You have " + secondsLeft + " seconds remaining";
+
+             if(secondsLeft === 0) {
+             clearInterval(timerInterval);
+             //GAME OVER. Need function to display score, then redirect to leaderboard
+         }
+       }, 1000);
      }
- }
+    }
+    setTime (); 
+    }
+    
+     
